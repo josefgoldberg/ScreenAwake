65326dbcbb35450f2ed01dee50822b8c3b963b8c
Josef Goldberg <goldbergjosef@gmail.com>
2019-09-27 11:22:15 -0400

    added terminal status function

--- windows_mouse.py
+++ windows_mouse.py
@@ -1,16 +1,30 @@
-print("Starting up... Hold on...")
-
 import pyautogui
 import webbrowser
 import time
 import os
+import time
+import requests
+from lxml import html
+import sys
+from selenium import webdriver
 
 def onlyMove():
     print('Keep the mouse in the top left corner to stop the Move function.')
     currentMouseX, currentMouseY = pyautogui.position()
     while(currentMouseX != 0 and currentMouseY != 0):
         currentMouseX, currentMouseY = pyautogui.position()
-        pyautogui.moveTo(currentMouseX+3, currentMouseY)
+        if currentMouseX >= 1000:
+            directionX = -20
+        else:
+            directionX = 20
+        if currentMouseY >= 500:
+            directionY = -20
+        else:
+            directionY = 20
+        if currentMouseX + directionX == 1 and currentMouseY + directionY ==1:
+            directionX = 100
+            directionY = 100
+        pyautogui.moveTo(currentMouseX+directionX, currentMouseY+directionY, duration=1)
         time.sleep(10)
         currentMouseX, currentMouseY = pyautogui.position()
 
@@ -28,9 +42,33 @@ def rotateTabs():
             i = 0
             time.sleep(30)
         pyautogui.click(100 + 210*i,20, duration=1)
-        #pyautogui.click(100, 60, duration=0)
-        pyautogui.hotkey("ctrl", "r")
+        pyautogui.click(100, 60, duration=0)
+        #pyautogui.hotkey("ctrl", "r")
         i += 1
+        time.sleep(10)
+        currentMouseX, currentMouseY = pyautogui.position()
+
+def termStatus():
+    currentMouseX, currentMouseY = pyautogui.position()
+    while(currentMouseX != 0 and currentMouseY != 0):
+        os.system('cls')
+        urls = ["http://128.226.95.31/prnLcdStatus.dhtml", "http://128.226.95.32/prnLcdStatus.dhtml", "http://128.226.95.34/home/index.html#hashHome", "http://128.226.95.33/prnLcdStatus.dhtml", "http://128.226.162.41/prnLcdStatus.dhtml", "http://128.226.162.42/prnLcdStatus.dhtml", "http://128.226.162.43/home/index.html#hashHome", "http://128.226.61.36/prnLcdStatus.dhtml"]
+        labels = ["AAG06-1", "AAG06-2", "AAG06-C", "AAG14", "SLIC-1", "SLIC-2", "SLIC-C", "S3G13"]	
+        for i in range(0,8):
+            try:
+                if i==2 or i==6:
+                    #TODO find solution for color printers
+                    raise Exception('Color Printers not yet compatible.')
+                    print(labels[i], "-", page.xpath('//label[@class="xux-labelableBox-label"]/text()'))
+                else:
+                    test = requests.get(urls[i])
+                    page = html.fromstring(test.content)
+                    print(labels[i], "-", page.xpath('//td[@class="myAttributeDescriptor"]/text()'))
+            except:
+                print(labels[i], "-", "Status Unavailable")
+                #print(sys.exc_info())
+        print('')
+        print('Keep the mouse in the top left corner to stop the status function.')
         time.sleep(10)
         currentMouseX, currentMouseY = pyautogui.position()
 
@@ -39,6 +77,7 @@ def printCommandList():
     print("    'Move' - keeps the computer awake by moving the mouse")
     print("    'Open' - opens each printer status link")
     print("    'Rotate' - rotates between all printer status links")
+    print("    'Status' - shows the status of all BW printers in terminal")
     print("    'Quit' - closes the program")
 
 linkOpen = False
@@ -58,9 +97,9 @@ while(True):
         else:
             webbrowser.get()
             rotateTabs()
+    elif inp == 'Status':
+        termStatus()
     elif inp == 'Quit':
         break
     else:
         print("Invalid Input")
-        
-        
